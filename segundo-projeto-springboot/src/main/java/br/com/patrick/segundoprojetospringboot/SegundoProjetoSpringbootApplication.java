9c412d88d292f2bde0d061cf28ea429c0fbe03e5
Patrick Rodrigues <patrickdevbjj6@hotmail.com>
2023-10-26 15:34:53 -0300

    Iniciando meu novo projeto com o SpringBoot

---
+++ segundo-projeto-springboot/src/main/java/br/com/patrick/segundoprojetospringboot/SegundoProjetoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.patrick.segundoprojetospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoProjetoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoProjetoSpringbootApplication.class, args);
+	}
+
+}
